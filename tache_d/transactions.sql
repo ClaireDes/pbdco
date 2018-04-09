7f7bad135a367d5aafeba5526995c0cb78b1e588
Claire Deshayes <claire.deshayes@phelma.grenoble-inp.fr>
2018-04-09 16:58:07 +0200

    Debut des transactions et requetes en SQL

--- tache_d/transactions.sql
+++ tache_d/transactions.sql
@@ -1,9 +1,19 @@
 /****TRANSACTIONS****/
 
 /* Créer joueur */
+INSERT INTO Joueurs(codeJoueur, Prenom, Nom, Adresse) VALUES (id_louis, 'Louis','Martin', '32 rue des marguerites, 12345 JolieVille');
 
 /* Créer rencontre */
+INSERT INTO Rencontres(codeTour, codeRencontre, JoueurBlanc, JoueurNoir) VALUES ('finale',1,21,16);
 
 /* Rejouer partie */
 
-/* */
+/* Supprimer l'historique */
+DELETE FROM Coups WHERE codeRencontre=rencontreARejouer;
+
+/* Mettre à jour JoueurBlanc, JoueurNoir */
+UPDATE Rencontre SET (JoueurBlanc=nouveauJBlanc) WHERE codeRencontre=rencontreARejouer;
+
+/* Créer coup */
+INSERT INTO Coups VALUES (codeCoup, LigneCourante, ColonneCourante, LignePrec, ColonnePrec);
+
