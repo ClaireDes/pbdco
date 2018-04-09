49cfff16643d0590c1840cafe2335e7b8edabe09
Claire Deshayes <claire.deshayes@phelma.grenoble-inp.fr>
2018-04-09 15:23:54 +0200

    Ajout fichier des requetes a faire

---
+++ tache_d/requetes.sql
@@ -0,0 +1,14 @@
+/****REQUETES****/
+
+/* nombre joueurs */
+
+/* nombre parties gagnées par joueur */
+
+/* nombre parties jouées par joueur */
+
+/* liste parties par tour */
+
+/* liste parties à jouer / en train d'être jouées par tour */
+
+
+
