2a50ff1966af5912da8dda0a206a70c5ca005458
Quentin Ravenet Provent <quentin.ravenet@phelma.grenoble-inp.fr>
2018-04-23 15:35:51 +0200

    Creation de fichiers pour la vue ET ajout de ControleurTournois.java

---
+++ PBDCO/src/pbdco/vue/VueTournois.java
@@ -0,0 +1,13 @@
+public class VueTournois {
+    private Grille grille;
+    private BarreOutils barreOutils;
+    private Joueur joueurBlanc;
+    private Joueur joueurNoir;
+
+    public VueOrga(Joueur joueurBlanc, Joueur joueurNoir) {
+        this.joueurBlanc = joueurBlanc;
+        this.joueurNoir = joueurNoir;
+    }
+
+
+}
