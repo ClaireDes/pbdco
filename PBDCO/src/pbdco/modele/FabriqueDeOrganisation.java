a962917365557f881c83d9a0d1d7f0259e78efb1
ClaireDes <claire.deshayes@phelma.grenoble-inp.fr>
2018-05-03 21:41:04 +0200

    implementation nbrDeJoueurs

--- PBDCO/src/pbdco/modele/FabriqueDeOrganisation.java
+++ PBDCO/src/pbdco/modele/FabriqueDeOrganisation.java
@@ -5,7 +5,9 @@
  */
 package pbdco.modele;
 
+import java.sql.*;
 import pbdco.*;
+import static pbdco.modele.FabriqueTransaction.*;
 
 
 /**
@@ -13,12 +15,51 @@ import pbdco.*;
  * @author milcenan
  */
 public class FabriqueDeOrganisation {
-    public int nbrDeJoueurs(Code codeTournois){
+    public int nbrDeJoueurs(Code codeTournois) throws BDAccessEx{
         //ici la requète pour
         //appel à la BD concernant la table TOUR
         //pour connaitre le nombre de joueurs dans le tournois
-        throw new UnsupportedOperationException("Not supported yet.");
+        String requete = "SELECT COUNT(codeJoueur) FROM Joueur NATURAL JOIN Rencontre WHERE codeTour=?;";
+        ResultSet resultat;
+        int nbJoueurs=0;
+        
+        try{// Chragement du Driver
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+        }catch( SQLException ex){
+            throw new BDAccessEx("nbrDeJoueurs Raised classNotFound exception "
+                    + "during the driver loading"+ex.getMessage());
         }
+        System.out.println("Driver ok");
+        
+         // Connexion à la BD
+         try{
+            Connection conn = DriverManager.getConnection(URL, USER, PASSWD);
+            System.out.println("Connection ok");
+             try{
+            conn.setAutoCommit(false);
+            conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+            
+            //préparation de la requète
+            PreparedStatement pstmt = conn.prepareStatement(requete);
+            pstmt.setString(1, "qualif");
+            
+            resultat = pstmt.executeQuery();
+            nbJoueurs = resultat.getInt(1);
+            
+            conn.commit();
+            conn.close();
+           System.out.println("Le nombre de participants au tournoi est : " + nbJoueurs);         
+             }catch(  SQLException ex){//si la transaction echoue
+                conn.close();
+                 System.err.println(ex.getMessage());
+             }
+        }catch(  SQLException ex){
+            throw new BDAccessEx("nbrDeJoueurs Raised SQLException during the connection\n"+ ex.getMessage());
+        }
+         
+         return nbJoueurs;
+    
+    }
     
     public String quelTour(Code codeTournoi){
         
