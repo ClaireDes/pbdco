b7dffd9d33c1706ce956aba4a2c04604c321679b
Barbault Thomas <thomas.barbault@phelma.grenoble-inp.fr>
2018-05-03 11:34:31 +0200

    ajout de requetes

--- tache_d/requetes.sql
+++ tache_d/requetes.sql
@@ -27,3 +27,38 @@ WHERE codeJouer=Machin;
 SELECT codeRencontre from Rencontres 
 MINUS 
 (SELECT codeRencontre from Vainqueur); 
+
+
+/*Position possible ? */
+/* la colonne CASE ainsi créee indique si la case est atteignable par la pièce ou non*/
+SELECT ligneFin, colonneFin 
+FROM Pieces 
+CASE
+	WHEN ligneFin > 8 OR ligneFin < 0 OR colonneFin < 0 OR colonneFin > 8 THEN 'false'
+	WHEN type = 'tour' AND ligneFin=ligneInit THEN 'true' /*on suppose qu'on se deplace*/
+	WHEN type = 'tour' AND colonneFin=colonneInit THEN 'true'
+	WHEN type = 'tour' AND ligneFin <> ligneInit AND colonneFin <> colonneFin THEN 'false'
+	WHEN type = 'fou' AND ABS(ligneInit-ligneFin) = ABS(colonneInit-colonneFin) THEN 'true'
+	WHEN type = 'fou' AND ABS(ligneInit-ligneFin) <> ABS(colonneInit-colonneFin) THEN 'false'
+	WHEN type = 'cavalier' AND ABS(ligneInit-ligneFin)=2 AND ABS(colonneInit-colonneFin)=1 THEN 'true'
+	WHEN type = 'cavalier' AND ABS(ligneInit-ligneFin)=1 AND ABS(colonneInit-colonneFin)=2 THEN 'true'
+	WHEN type = 'cavalier' AND ABS(ligneInit-ligneFin)=1 AND ABS(colonneInit-colonneFin)<>2 THEN 'false'
+	WHEN type = 'cavalier' AND ABS(ligneInit-ligneFin)=2 AND ABS(colonneInit-colonneFin)<>1 THEN 'false'
+	WHEN type = 'cavalier' AND ABS(ligneInit-ligneFin)<>1 AND ABS(colonneInit-colonneFin)=2 THEN 'false'
+	WHEN type = 'cavalier' AND ABS(ligneInit-ligneFin)<>2 AND ABS(colonneInit-colonneFin)=1 THEN 'true'
+	WHEN type = 'cavalier' AND ABS(ligneInit-ligneFin)>1 THEN 'false'
+	WHEN type = 'cavalier' AND ABS(colonneInit-colonneFin)>1 THEN 'false'
+	WHEN type = 'roi' AND ABS(ligneInit-ligneFin)>1 THEN 'false'
+	WHEN type = 'roi' AND ABS(colonneInit-colonneFin)>1 THEN 'false'
+	WHEN type = 'roi' AND ABS(ligneInit-ligneFin)=1 OR ABS(colonneInit-colonneFin)=1 THEN 'true'
+	WHEN type = 'reine' AND ABS(ligneInit-ligneFin) = ABS(colonneInit-colonneFin) THEN 'true'
+	WHEN type = 'reine' AND ligneFin=ligneInit THEN 'true'
+	WHEN type = 'reine' AND colonneFin=colonneInit THEN 'true'
+ELSE 'false';
+	
+		
+
+
+
+
+/*position => couleur */
