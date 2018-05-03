76217e1a7b351771725053783822e7b4eeebb093
Quentin Ravenet Provent <quentin.ravenet@phelma.grenoble-inp.fr>
2018-05-03 14:30:45 +0200

    pull

--- PBDCO/src/pbdco/vue/VueTournois.java
+++ PBDCO/src/pbdco/vue/VueTournois.java
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
 import pbdco.modele.*;
 
-public class VueTournois {
-    //private Grille grille;
-    //private BarreOutils barreOutils;
-=======
 package pbdco.vue;
 
 public class VueTournois {
     /*private Grille grille;
     private BarreOutils barreOutils;
->>>>>>> acd19520c094ff944dcb62db212c82f1f24a8e9e
     private Joueur joueurBlanc;
     private Joueur joueurNoir;
 
