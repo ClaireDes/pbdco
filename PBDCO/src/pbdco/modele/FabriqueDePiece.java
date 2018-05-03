bef885d4d4c7a1c1a6a19bdacb44578d38b7e46f
ClaireDes <claire.deshayes@phelma.grenoble-inp.fr>
2018-05-03 21:59:36 +0200

    implementation nouvellepiece

--- PBDCO/src/pbdco/modele/FabriqueDePiece.java
+++ PBDCO/src/pbdco/modele/FabriqueDePiece.java
@@ -5,8 +5,10 @@
  */
 package pbdco.modele;
 
+import java.sql.*;
 import pbdco.partie.*;
 import pbdco.*;
+import static pbdco.modele.FabriqueTransaction.*;
 
 /**
  *
@@ -14,21 +16,71 @@ import pbdco.*;
  */
 public class FabriqueDePiece {
     
-     public  void fabriqueTransaction(String operation,Piece piece){
+    public  void fabriqueTransaction(String operation,Piece piece){
         switch (operation) {
            
-            case "move" : //déplacement d'une pièce dans la base de données à l'occasiond'un coup 
+            //déplacement d'une pièce dans la base de données à l'occasiond'un coup
+            case "move" :  
                     System.out.println("enregistrement d'une pièce \""+ piece.getNom() +"\" dans la base");
                 break;
         }
      }
      
-     public void nouvellePiece(Piece piece, Code codeTour, Code codeRencontre){//création d'une pièce correspondant à la pièce passée en parametre
+     //création d'une pièce correspondant à la pièce passée en parametre
+    public void nouvellePiece(Piece piece, Code codeTour, Code codeRencontre) throws BDAccessEx{
+        
+        //ajouter codePiece ??
+        String requete = "INSERT INTO Piece(ligneInit, colonneInit, "
+                + "ligneFin, colonneFin, typePiece, couleur, codeRencontre, codeTour)"
+                + "VALUES ?,?,?,?,?,?,?,?";
+        
+        try{// Chragement du Driver
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+        }catch( SQLException ex){
+            throw new BDAccessEx("nouvellePiece Raised classNotFound exception "
+                    + "during the driver loading"+ex.getMessage());
+        }
+        System.out.println("Driver ok");
+        
+         // Connexion à la BD
+         try{
+            Connection conn = DriverManager.getConnection(URL, USER, PASSWD);
+            System.out.println("Connection ok");
+             try{
+            conn.setAutoCommit(false);
+            conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+            
+            //préparation de la requète
+            PreparedStatement pstmt = conn.prepareStatement(requete);
+            pstmt.setInt(1, piece.getPreviousPosition().getX());
+            pstmt.setInt(2, piece.getPreviousPosition().getY());
+            pstmt.setInt(3, piece.getCurrentPosition().getX());
+            pstmt.setInt(4, piece.getCurrentPosition().getY());
+            pstmt.setString(5, piece.getNom());
+            pstmt.setString(6,piece.getCouleur());
+            pstmt.setInt(7,codeRencontre.getValue());
+            pstmt.setInt(8,codeTour.getValue());
+            
+            pstmt.executeUpdate();
+            
+            conn.commit();
+            conn.close();
+            
+            System.out.println("enregistrement d'une pièce \""+ piece.getNom() +"\" dans la base");
+
+            }catch(  SQLException ex){//si la transaction echoue
+                conn.close();
+                System.err.println(ex.getMessage());
+            }
+        }catch(  SQLException ex){
+            throw new BDAccessEx("nouvellePiece Raised SQLException during the connection\n"+ ex.getMessage());
+        }
+        
+        
          
-        System.out.println("enregistrement d'une pièce \""+ piece.getNom() +"\" dans la base");
      }
     
-      public void bougePiece(Piece piece,Coup coup, Code codeTour, Code codeRencontre){//création d'une pièce correspondant à la pièce passée en parametre
+     public void bougePiece(Piece piece,Coup coup, Code codeTour, Code codeRencontre){//création d'une pièce correspondant à la pièce passée en parametre
          
         System.out.println("enregistrement du déplacement de la pièce \""+ piece.getNom() +"\"  à partir de " + coup.getPreviousY(piece) +"," +coup.getPreviousX(piece));
      }
