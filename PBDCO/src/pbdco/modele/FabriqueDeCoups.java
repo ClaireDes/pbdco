b1b1ade4c994c7b75ad7868bdad064cda75abed9
Claire Deshayes <claire.deshayes@phelma.grenoble-inp.fr>
2018-05-04 16:33:29 +0200

    derniercode fabCoup fait

--- PBDCO/src/pbdco/modele/FabriqueDeCoups.java
+++ PBDCO/src/pbdco/modele/FabriqueDeCoups.java
@@ -9,35 +9,71 @@ import pbdco.*;
 import pbdco.partie.*;
 import java.sql.*;
 import javax.sql.*;
-
+import static pbdco.modele.FabriqueTransaction.*;
 
 /**
  *
  * @author milcenan
  */
-public class FabriqueDeCoups/* extends FabriqueTransaction*/{
-  
-   
-    public Code lastCodeBD() throws BDAccessEx{
+public class FabriqueDeCoups/* extends FabriqueTransaction*/ {
+
+    public Code lastCodeBD() throws BDAccessEx {
+        Code code;
+        ResultSet resultat;
+        String requete = "SELECT MAX(codeCoup) FROM Coup";
+        System.out.println("Recherche du dernier code Coup");
+
+        try {
+            Connection conn = DriverManager.getConnection(URL, USER, PASSWD);
+            try {
+                conn.setAutoCommit(false);
+                conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+
+                PreparedStatement pstmt;
+
+                System.out.println("statement  ok : " + requete);
+
+                pstmt = conn.prepareStatement(requete);
+                resultat = pstmt.executeQuery();
+                resultat.next();
+
+                System.out.println("requete ok");
+                code = new Code();
+                code.setValue(resultat.getInt(1));
+
+                conn.commit();
+                conn.close();
+
+                System.out.println("Le plus grand codeCoup Enregistré est " + code.getValue());
+                return code;
+
+            } catch (SQLException ex) {//si la transaction echoue
+                conn.rollback();
+                conn.close();
+                throw new BDAccessEx("lastCodeCoup() Raised SQLException during the transaction" + ex.getMessage());
+            }
+        } catch (SQLException ex) {
+            throw new BDAccessEx("lastCodeCoup() Raised SQLException during the connection" + ex.getMessage());
+        }
+
+    }
+
+    public void fabriqueRequete(String operation, Coup coup) {
+
         throw new UnsupportedOperationException("Not supported yet.");
     }
-    
-    public  void fabriqueRequete(String operation, Coup coup){
-        
-        throw new UnsupportedOperationException("Not supported yet.");
-    };    
+
+    ;    
 
 
     public void fabriqueTransaction(String operation, Coup coup) {
-         switch (operation) {
+        switch (operation) {
             case "new"://création d'un coup 
-                    System.out.println("enregistrement d'un nouveau joueur dans la base");
+                System.out.println("enregistrement d'un nouveau joueur dans la base");
                 break;
-                
-         
-         }
-         
-    }
-    
-}
 
+        }
+
+    }
+
+}
